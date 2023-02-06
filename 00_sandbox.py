7b2f42b7939a97ae3a1bd89524847764e7c683b7
raylebis <100240815+raylebis@users.noreply.github.com>
2023-02-07 11:36:20 +1300

    Update 00_sandbox.py

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,11 +1,9 @@
-# Ask users for a number
-get_number = int(input("Choose a number? "))
+# Count DOWN from one to 10
 
-# Multiply the number by 5
-times_five = get_number * 5
+name = ""
+while name.lower() != "xxx":
+    name = input("Who are you? ")
+    print(name)
 
-answer = "{} times five is equal to " \
-         "{}".format(get_number, times_five)
-
-# Output the result
-print(answer)
+print()
+print("We are done!")
